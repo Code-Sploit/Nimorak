7190ebe0fbdc1421970b7110501138f43df71710
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2025-08-01 12:04:47 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,44 +1,33 @@
-#include "nimorak.h"
 #include "board.h"
-#include "constants.h"
-#include "search.h"
 #include "movegen.h"
-#include "helper.h"
 #include "perft.h"
 #include "attack.h"
+#include "table.h"
+#include "magic.h"
 
 #include <stdio.h>
-#include <ctype.h>
+
 #include <string.h>
-#include <stdbool.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
 
-#define POSITION_STARTPOS_FEN "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-
-int get_random(int min, int max) {
-    return rand() % (max - min + 1) + min;
+int rand_between(int a, int b)
+{
+    srand(time(NULL));
+    return a + rand() % (b - a);
 }
 
-void uci_loop(GameState *game)
+void uci_loop(Game *game)
 {
-    if (!game) return;
+    char input[4096];
 
-    char line[4096];
-
-    while (1)
+    while (fgets(input, sizeof(input), stdin))
     {
-        fflush(stdout);
+        // Remove newline characters
+        input[strcspn(input, "\r\n")] = 0;
 
-        if (!fgets(line, sizeof(line), stdin)) continue;
-
-        line[strcspn(line, "\n")] = 0;
-
-        if (strncmp(line, "ucinewgame", 10) == 0)
-        {
-            board_clear(game);
-        }
-        else if (strcmp(line, "uci") == 0)
+        if (strcmp(input, "uci") == 0)
         {
             printf("id name Nimorak\n");
             printf("id author Samuel 't Hart\n");
@@ -46,126 +35,106 @@ void uci_loop(GameState *game)
 
             fflush(stdout);
         }
-        else if (strcmp(line, "isready") == 0)
+        else if (strcmp(input, "isready") == 0)
         {
             printf("readyok\n");
 
             fflush(stdout);
         }
-        else if (strncmp(line, "position", 8) == 0)
+        else if (strncmp(input, "position", 8) == 0)
         {
-            char *ptr = line + 9;
+            char *ptr = input + 9;
 
             if (strncmp(ptr, "startpos", 8) == 0)
             {
-                board_load_fen(game, POSITION_STARTPOS_FEN);
+                board_load_fen(game, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
                 ptr += 8;
             }
             else if (strncmp(ptr, "fen", 3) == 0)
             {
-                ptr += 3;
-
-                while (*ptr && isspace(*ptr)) ptr++;
-
-                char fen[128] = {0};
-                int i = 0;
-                int spaces = 0;
-
-                // FEN has exactly 6 space-separated fields
-                while (*ptr && spaces < 6 && i < (int)(sizeof(fen) - 1))
+                ptr += 4;
+                char fen[4096];
+                strcpy(fen, ptr);
+                
+                // Stop at "moves" if present
+                char *moves_pos = strstr(fen, " moves");
+                if (moves_pos)
                 {
-                    if (isspace(*ptr))
-                        spaces++;
-
-                    fen[i++] = *ptr++;
+                    *moves_pos = '\0';
                 }
-
-                fen[i] = '\0';
+                
                 board_load_fen(game, fen);
             }
 
-            // Now parse optional moves
-            while (*ptr && isspace(*ptr)) ptr++;
-
-            if (strncmp(ptr, "moves", 5) == 0)
+            // Handle moves
+            char *moves = strstr(ptr, "moves");
+            if (moves)
             {
-                ptr += 5;
-
-                while (*ptr)
+                moves += 6; // skip "moves "
+                while (*moves)
                 {
-                    while (*ptr && isspace(*ptr)) ptr++;
+                    char move_str[6];
+                    sscanf(moves, "%s", move_str);
 
-                    if (!*ptr) break;
+                    Move move = board_parse_move(game, move_str);
 
-                    char move_str[6] = {0};
-                    int j = 0;
+                    board_make_move(game, move);
 
-                    while (*ptr && !isspace(*ptr) && j < 5)
-                    {
-                        move_str[j++] = *ptr++;
-                    }
-
-                    move_str[j] = '\0';
-
-                    board_make_move_str(game, move_str);
+                    // Advance to next move
+                    moves += strcspn(moves, " ");
+                    while (*moves == ' ') moves++;
                 }
             }
 
-            attack_generate_table(game, WHITE);
-            attack_generate_table(game, BLACK);
-        }
-        else if (strncmp(line, "go", 2) == 0)
-        {
-            movegen_generate_legal_moves(game);
-
-            int index = get_random(0, game->move_count);
-
-            Move move = game->movelist[get_random(0, game->move_count - 1)];
-
-            char *promotion_string = (move.promotion) ? piece_to_char(move.promotion_piece) : " ";
-
-            printf("bestmove %s%s%s\n", translate_square_to_string(move.from), translate_square_to_string(move.to), promotion_string);
+            attack_print_table(game, WHITE);
 
             fflush(stdout);
         }
-        else if (strncmp(line, "perft_tp", 8) == 0)
+        else if (strncmp(input, "go perft ", 9) == 0)
         {
-            perft_run_test_positions(game);
+            int depth = atoi(input + 9);
+            perft_root(game, depth);
         }
-        else if (strncmp(line, "perft", 5) == 0)
+        else if (strncmp(input, "go perfttest", 12) == 0)
         {
-            int depth = 1;
-            char *ptr = line + 5;
-
-            // Skip whitespace after "perft"
-            while (*ptr && isspace(*ptr)) ptr++;
-
-            // If there's a number, parse it
-            if (*ptr)
-                depth = atoi(ptr);
-
-            perft_divide(game, depth, true);
+            perft_run_tests(game);
         }
-        else if (strncmp(line, "atw", 3) == 0)
+        else if (strncmp(input, "go", 2) == 0)
+        {
+            movegen_generate_legal_moves(game);
+
+            printf("bestmove %s\n", board_move_to_string(game->movelist[rand_between(0, game->move_count)]));
+
+            fflush(stdout);
+        }
+        else if (strcmp(input, "quit") == 0)
+        {
+            break;
+        }
+        else if (strcmp(input, "display") == 0)
+        {
+            board_print(game);
+
+            fflush(stdout);
+        }
+        else if (strcmp(input, "atw") == 0)
         {
             attack_print_table(game, WHITE);
         }
-        else if (strncmp(line, "atb", 3) == 0)
+        else if (strcmp(input, "atb") == 0)
         {
             attack_print_table(game, BLACK);
-        }
-        else if (strncmp(line, "d", 1) == 0)
-        {
-            board_print(game);
         }
     }
 }
 
-int main(int argc, char** argv)
+int main()
 {
-    GameState *game = game_new();
+    srand(1234);
 
-    nimorak_startup();
+    Game *game = game_new();
+
+    table_precompute_all_attacks(game);
 
     uci_loop(game);
 
