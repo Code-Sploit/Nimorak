590d46190d90ce4e5934e6290a5f478d90243461
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2025-08-01 12:05:03 +0200

    Update movegen.h

--- movegen.h
+++ movegen.h
@@ -3,8 +3,24 @@
 
 #include "nimorak.h"
 
-void movegen_generate_pseudo_legal_moves(GameState *game);
+extern int BISHOP_OFFSETS[4];
+extern int ROOK_OFFSETS[4];
+extern int QUEEN_OFFSETS[8];
+extern int KING_OFFSETS[8];
 
-void movegen_generate_legal_moves(GameState *game);
+/* Offsets for pawns (inverted for black) */
+
+extern int PAWN_OFFSETS[4];
+
+/* Offsets for knights */
+
+extern int KNIGHT_OFFSETS[8];
+
+void movegen_generate_pawn_moves(Game *game);
+void movegen_generate_knight_moves(Game *game);
+void movegen_generate_sliding_moves(Game *game, int piece_type);
+
+void movegen_generate_pseudo_moves(Game *game);
+void movegen_generate_legal_moves(Game *game);
 
 #endif
