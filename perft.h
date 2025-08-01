ab2f10b78d7ea720c0dee6f359e4c9bf1104c1da
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2025-08-01 12:01:07 +0200

    Update perft.h

--- perft.h
+++ perft.h
@@ -1,39 +1,10 @@
 #ifndef PERFT_H
 #define PERFT_H
 
-#include "board.h"
+#include "nimorak.h"
 
-typedef struct {
-    long long nodes;
-    long long captures;
-    long long promotions;
-    long long castles;
-    long long en_passants;
-    long long checks;
-    long long quiet_moves;
-} PerftResult;
-
-typedef struct {
-    int depth;
-    uint64_t expected;
-} DepthExpectation;
-
-typedef struct {
-    const char *fen;
-    DepthExpectation expectations[5];  // Adjust number depending on how many depths you want per position
-    int num_expectations;
-} PerftTest;
-
-long long perft(GameState *game, int depth);
-
-uint64_t perft_divide(GameState *game, int depth, int debug);
-
-void perft_debug(GameState *game, int depth, const char *root_move_str);
-
-PerftResult perft_detailed(GameState *game, int depth);
-
-void perft_print_breakdown(GameState *game, int depth);
-
-void perft_run_test_positions(GameState *game);
+long long perft(Game *game, int depth);
+void perft_run_tests(Game *game);
+void perft_root(Game *game, int depth);
 
 #endif
