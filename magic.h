09b3308a5dad9b7774a8949572a554463502178e
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2025-08-01 12:06:13 +0200

    Create magic.h

---
+++ magic.h
@@ -0,0 +1,21 @@
+#ifndef MAGIC_H
+#define MAGIC_H
+
+#include "nimorak.h"
+
+#define BISHOP_ATTACK_TABLE_SIZE 5248
+#define ROOK_ATTACK_TABLE_SIZE 102400
+
+extern Bitboard bishop_attack_table[BISHOP_ATTACK_TABLE_SIZE];
+extern Bitboard rook_attack_table[ROOK_ATTACK_TABLE_SIZE];
+
+Magic* generate_bishop_magics();
+Magic* generate_rook_magics();
+
+Bitboard magic_get_bishop_attacks(Game *game, int square, Bitboard occupancy);
+Bitboard magic_get_rook_attacks(Game *game, int square, Bitboard occupancy);
+
+void magic_best_bishops();
+
+void magic_best_rooks();
+#endif
